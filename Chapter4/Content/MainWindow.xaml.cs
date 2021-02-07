0c296f897ebf9235adbcf965b761bd428085762f
Myster-Sneppi <60942937+Alex-ViGiL@users.noreply.github.com>
2021-02-07 16:40:15 +0200

    Padding | Horizontal/VerticalAligenment

---
+++ Chapter4/Content/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Content
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StackPanel stackPanel = new StackPanel();
+
+            stackPanel.Children.Add(new TextBox { Text = " hey" , Background = new SolidColorBrush (Colors.Red), Width = 20, Height = 20 });
+            stackPanel.Children.Add(new Button { Content = "Press", FontSize = 22, Height = 50, Cursor = Cursors.Arrow });
+
+            b.Content = stackPanel;
+
+        }
+    }
+}
